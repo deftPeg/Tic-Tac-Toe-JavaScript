655f05323ac566720c341fc689377bc3f3d98388
Peggy Kalliani <pegztales@gmail.com>
2023-02-22 23:44:31 +0200

    changing interface adding gif image

--- main.js
+++ main.js
@@ -1,107 +1,120 @@
-// assign status display from DOM to variable.
-let statusDisplay = document.querySelector('.gameStatus');
+//Adding x and circle classes
+const X_CLASS = "x"
+const CIRCLE_CLASS = "circle"
 
-// assign state of game, active player x turn and empty cells.
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
+//Declaring winning combinations
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
-// declare messages for win, draw and current player.
-let winningMessage = () => `Player ${currentPlayer} has won!`;
-let drawMessage = () => `The game ended in a draw!`;
-let currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+//Declaring data attributes and classes
+const cellElements = document.querySelectorAll("[data-cell]")
+const board = document.getElementById("board")
+const winningMessageElement = document.getElementById("winningMessage")
+const restartButton = document.getElementById("restart")
+const winImage = document.getElementById("winImage")
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+)
+let circleTurn
 
-// display the current player
-statusDisplay.innerHTML = currentPlayerTurn();
+//Start game as soon as the page loads
+startGame()
 
-// declare the winning combinations according to the cells.
-let winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+//Adding an event listener to restart button to start the game again
+restartButton.addEventListener("click", startGame)
 
-// function to handle the cell getting clicked and assign the proper player.
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
+//Creating startGame function
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener("click", userClick)
+    cell.addEventListener("click", userClick, { once: true })
+  })
+  hoverTurns()
+  winningMessageElement.classList.remove("show")
+  winImage.classList.remove("show")
 }
 
-// function to handle change of player's turn
-function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
+//Check whether the user clicks and also handle that whose turn it is
+function userClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    hoverTurns()
+  }
 }
 
-// function to see if game has been won
-function handleResultValidation() {
-    let roundWon = false;
-    // there are 7 winning combination indices. We iterate through each.
-    for (let i = 0; i <= 7; i++) {
-        let winCondition = winningConditions[i];
-        // check each of the combinations via index within index (index[i])
-        let a = gameState[winCondition[0]];
-        let b = gameState[winCondition[1]];
-        let c = gameState[winCondition[2]];
-        // if there is an empty cell, continue
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        // if all three are marked the same player, that player has won
-        if (a === b && b === c) {
-            roundWon = true;
-            break
-        }
-    }
-    // if round won, display winner message and change status to inactive
-    if (roundWon) {
-        statusDisplay.innerHTML = winningMessage();
-        gameActive = false;
-        return;
-    }
-    // if draw, (no empty cell) display draw message and change status to inactive
-    let roundDraw = !gameState.includes("");
-    if (roundDraw) {
-        statusDisplay.innerHTML = drawMessage();
-        gameActive = false;
-        return;
-    }
-
-    // invoke change player function
-    handlePlayerChange();
+//Check for winner. If none then draw match and restart
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerHTML =
+      "Draw"
+      winningMessageElement.classList.add("show")
+  } else {
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+    winningMessageTextElement.innerHTML = `${
+      circleTurn
+        ? "O"
+        : "X"
+    } wins!`
+    winImage.classList.add("show")
+    winningMessageElement.classList.add("show")
+  }
+  // winningMessageElement.classList.add("show")
 }
 
-// function that upon cell click, targets the cell that is clicked and gets the cell index
-function handleCellClick(clickedCellEvent) {
-    let clickedCell = clickedCellEvent.target;
-    let clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-    if (gameState[clickedCellIndex] !== "" || !gameActive) {
-        return;
-    }
-
-    // invoke assign player to cell upon click function
-    handleCellPlayed(clickedCell, clickedCellIndex);
-    // invoke function that checks if there is a winner
-    handleResultValidation();
+//Checking where the user actually clicks
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return (
+      cell.classList.contains(X_CLASS) ||
+      cell.classList.contains(CIRCLE_CLASS)
+    )
+  })
 }
 
-
-// function to restart the game. Reset cells, player x turn and active game status
-function handleRestartGame() {
-    gameActive = true;
-    currentPlayer = "X";
-    gameState = ["", "", "", "", "", "", "", "", ""];
-    statusDisplay.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+//Add X or O to the cell clicked
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
 }
 
-// add event listeners to the cells and the restart button
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.gameRestart').addEventListener('click', handleRestartGame);
+//Change Turns
+function swapTurns() {
+  circleTurn = !circleTurn
+}
 
+//Shows whose turn when hovering over cell
+function hoverTurns() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+//Check to find a winning combination
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combinations => {
+    return combinations.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
